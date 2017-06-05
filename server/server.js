ec891e180b3f42722ee57b4edb35da2aa82de684
Nhan Tran <trdainhan@gmail.com>
2017-06-05 17:28:25 +0700

    Config nodemon, babel preset

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import next from 'next'
 import mongoose from 'mongoose'
 const Schema = mongoose.Schema
+import bodyParser from 'body-parser'
 
 import Note from '../database/Note'
 import NoteBook from '../database/NoteBook'
@@ -14,34 +15,35 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
   const server = express()
 
+  // Request body parsing middleware should be above methodOverride
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: false }));
+
   // Connect to database
   mongoose.connect(database.uri)
 
   server.get('/notebooks', (req, res) => {
-    const note = new Note({
-      title: 'nhan',
-      content: 'Hello',
-      boot_id: '11'
-    })
-    note.save()
-    
-    res.json({
-      test: "tran dai nhan"
+    NoteBook.find({}, (err, notebooks) => {
+      if (err) {
+        console.log('Something wrong in /notebooks')
+        res.status(500).send('Something broke!')
+      }
+      res.json(notebooks)
     })
   })
 
   server.post('/noteBooks', (req, res) => {
     const { title } = req.body // ?
     const noteBook = new NoteBook({
-      title: title
+      title: title || 'Untitle'
     })
-    noteBook.save()
-    return res.json({
-      nhan: 'test'
+    noteBook.save().then((note) => {
+      res.status(200).send('Save successfully')
     })
   })
 
   server.post('/note', (req, res) => {
+    console.log(req.body)
     const note = new Note({
       title: '',
       content: '',
