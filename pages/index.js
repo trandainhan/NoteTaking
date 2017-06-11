21b0e8668ec8431399757a7f01f8087c33cbeb2a
Nhan Tran <trdainhan@gmail.com>
2017-06-11 09:39:24 +0700

    Fetch note when index page mounted

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,25 @@ import Head from 'next/head'
 import Link from 'next/link'
 import React, { Component } from 'react'
 import withRedux from 'next-redux-wrapper'
-import { initStore } from '../store'
+import fetch from 'axios'
+import { values } from 'lodash-fp'
 
+import { initStore } from '../store'
 import SplitView from '../components/SplitView'
 import List from '../components/List'
 import Header from '../components/Header'
+import TopBar from '../components/TopBar'
 
 import NotePreview from '../container/NotePreview'
 import NoteBook from '../container/NoteBook'
 import NoteView from '../container/NoteView'
 
-import TopBar from '../components/TopBar'
-
-import { values } from 'lodash-fp'
+import { fetchNoteBooks } from '../action'
 
 class IndexPage extends Component {
+  componentDidMount () {
+    this.props.dispatch(fetchNoteBooks())
+  }
   render () {
     const { noteBooks, selectedNote } = this.props
     return (
