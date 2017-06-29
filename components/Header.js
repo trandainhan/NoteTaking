d238e76ee9f095e2c3f9aa27ab83231953e4c2f7
Nhan Tran <trdainhan@gmail.com>
2017-06-29 22:01:15 +0700

    Implement NavBar Component

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 
 export default () => (
   <Head>
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
     <link rel="stylesheet" href="/static/css/base.css" />
   </Head>
---
+++ components/NavBar.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+import { getCookie, setCookie } from '../utils/CookieUtils'
+
+class NavBar extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { username: '' , showDropDown: false}
+    this.logout = e => this._logout()
+    this.showDropDown = e => this._showDropDown()
+  }
+  _logout () {
+    setCookie('username', '')
+    setCookie('x-access-token', '')
+    Router.push({
+      pathname: '/login'
+    })
+  }
+  _showDropDown () {
+    this.setState({
+      showDropDown: !this.state.showDropDown
+    })
+  }
+  componentDidMount () {
+    this.setState({
+      username: getCookie('username')
+    })
+  }
+  render () {
+    return (
+      <nav className="navbar navbar-default navbar-static-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link prefetch href='/'>
+              <a className="navbar-brand">NoteTaking</a>
+            </Link>
+          </div>
+
+          <div className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+            </ul>
+
+            <ul className="nav navbar-nav navbar-right">
+              <li className="dropdown">
+                <a
+                  className="dropdown-toggle"
+                  data-toggle="dropdown"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  {
+                    this.state.username
+                    ? <div onClick={this.showDropDown}>
+                        Hello {this.state.username}
+                        <span className="caret"></span>
+                      </div>
+                    : <Link href="/login"><p>Login</p></Link>
+                  }
+                </a>
+                {
+                  this.state.showDropDown &&
+                  <ul className="dropdown-menu displayBlock">
+                    <li><a href="#" onClick={this.logout}>Logout</a></li>
+                  </ul>
+                }
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default NavBar
